6310570abe7e458c430a94d92cf89f2ffeb31b6c
danny vincent <87720409+1oupd@users.noreply.github.com>
2021-10-28 14:38:21 +0100

    Navbar Styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,26 +1,25 @@
-import {Link} from 'react-router-dom';
-import './NavBar.css';
-
+import { Link } from "react-router-dom";
+import "./nav-bar.css";
 
 const Navbar = () => {
-    return(
-        <div className = "grandWrapper">
-        <nav className="navbar">
-            <h1>Cats for Life</h1>
-            <div className="links">
-                <div className = "soloLink">
-                    <Link to="/">Home</Link>   
-                </div>
-                <div className = "soloLink">
-                <Link to="/contact">Contact</Link>
-                </div>
-                <div className = "soloLink">
-                <Link to="/about">About</Link>
-                </div>
-            </div>
-        </nav>
+  return (
+    <div className="grandWrapper">
+      <nav className="navbar">
+        <h1>Cats for Life</h1>
+        <div className="links">
+          <div className="soloLink">
+            <Link to="/">Home</Link>
+          </div>
+          <div className="soloLink">
+            <Link to="/contact">Contact</Link>
+          </div>
+          <div className="soloLink">
+            <Link to="/about">About</Link>
+          </div>
+        </div>
+      </nav>
     </div>
-    );
-}
+  );
+};
 
 export default Navbar;
